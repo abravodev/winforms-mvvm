cbb76217c3b5c26033e36d4498e674fb07a6e922
A Bravo Dev <abravodev@users.noreply.github.com>
2020-12-04 21:42:44 +0100

    Fix finding elements based on its system element name, it depends on thread's culture

---
+++ WinformsTools.IntegrationTestUtils/Extensions/CustomLocalizedStrings.cs
@@ -0,0 +1,93 @@
+﻿using FlaUI.Core.Tools;
+using System.Threading;
+
+namespace WinformsTools.IntegrationTestUtils.Extensions
+{
+    /// <summary>
+    /// Inspired by <see cref="FlaUI.Core.Tools.LocalizedStrings"/>
+    /// </summary>
+    public static class CustomLocalizedStrings
+    {
+        static CustomLocalizedStrings()
+        {
+            switch (Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName)
+            {
+                // TODO: Create PR for changing original code to include Spanish
+                case "es":
+                    WinFormsUIA2VerticalScrollBarName = "Barra de desplazamiento vertical";
+                    WinFormsUIA3VerticalScrollBarName = "Vertical";  // TODO: Find out Spanish version
+                    WinFormsUIA2HorizontalScrollBarName = "Horizontal Scroll Bar";  // TODO: Find out Spanish version
+                    WinFormsUIA3HorizontalScrollBarName = "Horizontal";  // TODO: Find out Spanish version
+                    HorizontalScrollBar = "Horizontal ScrollBar";  // TODO: Find out Spanish version
+                    VerticalScrollBar = "Vertical ScrollBar";  // TODO: Find out Spanish version
+                    TableHorizontalScrollBar = "Horizontal Scroll Bar";  // TODO: Find out Spanish version
+                    TableVerticalScrollBar = "Vertical Scroll Bar";  // TODO: Find out Spanish version
+                    DataGridViewHeader = "Fila superior";
+                    DataGridViewHeaderItemTopLeft = "Celda de encabezado superior izquierda";
+                    break;
+                default:
+                    WinFormsUIA2VerticalScrollBarName = LocalizedStrings.WinFormsUIA2VerticalScrollBarName;
+                    WinFormsUIA3VerticalScrollBarName = LocalizedStrings.WinFormsUIA3VerticalScrollBarName;
+                    WinFormsUIA2HorizontalScrollBarName = LocalizedStrings.WinFormsUIA2HorizontalScrollBarName;
+                    WinFormsUIA3HorizontalScrollBarName = LocalizedStrings.WinFormsUIA3HorizontalScrollBarName;
+                    HorizontalScrollBar = LocalizedStrings.HorizontalScrollBar;
+                    VerticalScrollBar = LocalizedStrings.VerticalScrollBar;
+                    TableHorizontalScrollBar = LocalizedStrings.TableHorizontalScrollBar;
+                    TableVerticalScrollBar = LocalizedStrings.TableVerticalScrollBar;
+                    DataGridViewHeader = LocalizedStrings.DataGridViewHeader;
+                    DataGridViewHeaderItemTopLeft = LocalizedStrings.DataGridViewHeaderItemTopLeft;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Name of a horizontal scrollbar.
+        /// </summary>
+        public static string HorizontalScrollBar { get; }
+
+        /// <summary>
+        /// Name of a vertical scrollbar.
+        /// </summary>
+        public static string VerticalScrollBar { get; }
+
+        /// <summary>
+        /// Name of a horizontal scrollbar in a table.
+        /// </summary>
+        public static string TableHorizontalScrollBar { get; }
+
+        /// <summary>
+        /// Name of a vertical scrollbar in a table.
+        /// </summary>
+        public static string TableVerticalScrollBar { get; }
+
+        /// <summary>
+        /// Name of a WinForms vertical scrollbar in UIA2.
+        /// </summary>
+        public static string WinFormsUIA2VerticalScrollBarName { get; }
+
+        /// <summary>
+        /// Name of a WinForms vertical scrollbar in UIA3.
+        /// </summary>
+        public static string WinFormsUIA3VerticalScrollBarName { get; }
+
+        /// <summary>
+        /// Name of a WinForms horizontal scrollbar in UIA2.
+        /// </summary>
+        public static string WinFormsUIA2HorizontalScrollBarName { get; }
+
+        /// <summary>
+        /// Name of a WinForms horizontal scrollbar in UIA3.
+        /// </summary>
+        public static string WinFormsUIA3HorizontalScrollBarName { get; }
+
+        /// <summary>
+        /// Name of the header row in a DataGridView.
+        /// </summary>
+        public static string DataGridViewHeader { get; set; }
+
+        /// <summary>
+        /// Name of the top-left header item.
+        /// </summary>
+        public static string DataGridViewHeaderItemTopLeft { get; set; }
+    }
+}
--- WinformsTools.IntegrationTestUtils/Extensions/DataGridViewExtensions.cs
+++ WinformsTools.IntegrationTestUtils/Extensions/DataGridViewExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Definitions;
 using FlaUI.Core.Tools;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,12 @@ namespace WinformsTools.IntegrationTestUtils.Extensions
     {
         public static DataGridViewRow[] GetRows(this DataGridView table)
         {
-            return Retry.WhileEmpty(() => table.Rows).Result;
+            return Retry.WhileEmpty(() => GetDataGridViewRow(table)).Result;
         }
 
         public static IEnumerable<IDictionary<string, DataGridViewCell>> ToDictionary(this DataGridView table)
         {
-            var columns = table.Header.Columns.Select(x => x.Text).ToList();
+            var columns = GetCellNames(table);
             foreach (var row in table.Rows)
             {
                 yield return row.Cells.ToDictionary(columns);
@@ -24,11 +25,18 @@ namespace WinformsTools.IntegrationTestUtils.Extensions
         public static IEnumerable<IDictionary<string, DataGridViewCell>> ToDictionary(this DataGridViewRow[] rows)
         {
             var table = rows[0].Parent.AsDataGridView();
-            var columns = table.Header.Columns.Select(x => x.Text).ToList();
+            var columns = GetCellNames(table);
             foreach (var row in rows)
             {
                 yield return row.Cells.ToDictionary(columns);
             }
+        }
+
+        private static List<string> GetCellNames(DataGridView table)
+        {
+            var header = GetDataGridViewHeader(table);
+            var headerColumns = GetDataGridViewHeaderColumns(header);
+            return headerColumns.Select(x => x.Text).ToList();
         }
 
         public static IDictionary<string, DataGridViewCell> ToDictionary(this DataGridViewCell[] cells, IEnumerable<string> columns)
@@ -39,6 +47,60 @@ namespace WinformsTools.IntegrationTestUtils.Extensions
                 dictionary.Add(columns.ElementAt(i), cells[i]);
             }
             return dictionary;
+        }
+
+        /// <summary>
+        /// <see cref="DataGridView.Rows"/> does not work in culture different than English or German:
+        /// <see href="https://github.com/FlaUI/FlaUI/blob/master/src/FlaUI.Core/AutomationElements/DataGridView.cs"/>
+        /// <see href="https://github.com/FlaUI/FlaUI/blob/master/src/FlaUI.Core/Tools/LocalizedStrings.cs"/>
+        /// </summary>
+        /// <param name="dataGridView"></param>
+        /// <returns></returns>
+        private static DataGridViewRow[] GetDataGridViewRow(DataGridView dataGridView)
+        {
+            var rows = dataGridView.FindAllChildren(cf => cf.ByControlType(ControlType.Custom).Or(cf.ByControlType(ControlType.DataItem))
+                   .And(cf.ByName(CustomLocalizedStrings.DataGridViewHeader).Not()));
+            // Remove the last row if we have the "add" row
+            if (dataGridView.HasAddRow)
+            {
+                rows = rows.Take(rows.Length - 1).ToArray();
+            }
+            return rows.Select(x => new DataGridViewRow(x.FrameworkAutomationElement)).ToArray();
+        }
+
+        /// <summary>
+        /// <see cref="DataGridView.Header"/> does not work in culture different than English or German
+        /// <see href="https://github.com/FlaUI/FlaUI/blob/master/src/FlaUI.Core/AutomationElements/DataGridView.cs"/>
+        /// <see href="https://github.com/FlaUI/FlaUI/blob/master/src/FlaUI.Core/Tools/LocalizedStrings.cs"/>
+        /// </summary>
+        /// <param name="dataGridView"></param>
+        /// <returns></returns>
+        private static DataGridViewHeader GetDataGridViewHeader(DataGridView dataGridView)
+        {
+            var header = dataGridView.FindFirstChild(cf => cf.ByName(CustomLocalizedStrings.DataGridViewHeader).Or(cf.ByControlType(ControlType.Header)));
+            return header == null ? null : new DataGridViewHeader(header.FrameworkAutomationElement);
+        }
+
+        /// <summary>
+        /// <see cref="DataGridViewHeader.Columns"/> does not work in culture different than English or German
+        /// </summary>
+        /// <returns></returns>
+        private static DataGridViewHeaderItem[] GetDataGridViewHeaderColumns(DataGridViewHeader dataGridViewHeader)
+        {
+            // WinForms uses Header control type, WPF uses HeaderItem control type
+            var headerItems = dataGridViewHeader.FindAllChildren(cf => cf.ByControlType(ControlType.Header).Or(cf.ByControlType(ControlType.HeaderItem)));
+            var convertedHeaderItems = headerItems.Select(x => new DataGridViewHeaderItem(x.FrameworkAutomationElement))
+                .ToList();
+            // Remove the top-left header item if it exists (can be the first or last item)
+            if (convertedHeaderItems.Last().Text == CustomLocalizedStrings.DataGridViewHeaderItemTopLeft)
+            {
+                convertedHeaderItems = convertedHeaderItems.Take(convertedHeaderItems.Count - 1).ToList();
+            }
+            else if (convertedHeaderItems.First().Text == CustomLocalizedStrings.DataGridViewHeaderItemTopLeft)
+            {
+                convertedHeaderItems = convertedHeaderItems.Skip(1).ToList();
+            }
+            return convertedHeaderItems.ToArray();
         }
     }
 }
