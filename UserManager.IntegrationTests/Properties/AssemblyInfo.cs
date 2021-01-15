e265df301614af09e6f4940b5106c12d5a1e1365
A Bravo Dev <abravodev@users.noreply.github.com>
2021-01-15 20:36:17 +0100

    Include TestCategory for integration and unit tests in AssemblyInfo

--- UserManager.IntegrationTests/Properties/AssemblyInfo.cs
+++ UserManager.IntegrationTests/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -18,3 +19,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: TestCategory("IntegrationTests")]
+[assembly: TestCategory("SkipWhenLiveUnitTesting")]
